9a10b20b98f42b17668482f6fea9432b53c686e8
Allison <akcallah@gmail.com>
2023-07-07 10:12:41 -0700

    added additional home and avocado data template

--- avocado_project/avocado_data/urls.py
+++ avocado_project/avocado_data/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('housingprices', views.home_data, name="housing_prices"),
+    path('avocadoprices', views.avocado_data, name="avocado_prices"),
 ]
--- avocado_project/avocado_data/views.py
+++ avocado_project/avocado_data/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from datetime import datetime
 from .models import Avocado_Price
+from .models import City_House_Price
+
+def avocado_data(request):
+	price_list = Avocado_Price.objects.all()
+	return render(request, 'avocado_data/avocado_price_data.html', {"price_list" : price_list})
+
+def home_data(request):
+	price_list = City_House_Price.objects.all()
+	return render(request, 'avocado_data/house_price_data.html', {"price_list" : price_list})
 
 
 
